7091eda8f6a3053ba75e9583dd565d0d23940f3b
sjsu-koby <koby.nguyen@sjsu.edu>
2022-03-14 00:56:21 -0700

    changed a lot of files

--- sketch.js
+++ sketch.js
@@ -21,20 +21,28 @@ function setup() {
   background(200);
   textSize(32)
 
-  text("click to randomize", 50 , 50)
+  text("click to randomize", 50, 50)
 
+  setTimeout(changeBackground, 1000);
 
 }
 
+function draw() {
 
-function mousePressed(){
-  if(dogs[0]){
-  background(random(200, 255));
-  randomIndex = int(random(dogs.length));
-  text(dogs[randomIndex].name, 50, 50);
-  dogs.splice(randomIndex, 1);
-} else {
-  background(random(200, 255));
-  text("nothing left!", 50, 50);
 }
+
+function changeBackground() {
+  
+}
+
+function mousePressed() {
+  if (dogs[0]) {
+    background(random(200, 255));
+    randomIndex = int(random(dogs.length));
+    text(dogs[randomIndex].name, 50, 50);
+    dogs.splice(randomIndex, 1);
+  } else {
+    background(random(200, 255));
+    text("nothing left!", 50, 50);
+  }
 }
