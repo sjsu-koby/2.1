b82ccb7df1693bacf3a7000a1e72426413440879
sjsu-koby <koby.nguyen@sjsu.edu>
2022-03-16 00:18:32 -0700

    changed entire data to my data

--- sketch.js
+++ sketch.js
@@ -1,29 +1,48 @@
 // dogs I have met
-var dogs = [{
-  name: "Kira",
-  color: "grey"
+var dognames = [{
+  name: "Kira"
 }, {
-  name: "Kandy",
-  color: "brown"
+  name: "Kandy"
 }, {
-  name: "Stella",
-  color: "white"
+  name: "Stella"
 }, {
-  name: "Nalla",
-  color: "black"
+  name: "Nalla"
+}, {
+  name: "Holy"
 }];
 
+var randomImage;
 var randomIndex;
 var counter = 0;
+var introdogs;
+var dog0;
+var dog1;
+var dog2;
+var dog3;
+var dog4;
+var dogs = [];
+var dog = random(dogs);
+
+function preload() {
+
+  introdogs = loadImage(`images/multipledogs.jpg`);
+
+  for (let i = 0; i < 5; i++) {
+    dogs[i] = loadImage(`images/dog` + i + `.jpg`);
+  }
+  // dog0 = loadImage(`images/dog0.jpg`);
+  // dog1 = loadImage(`images/dog1.jpg`);
+  // dog2 = loadImage(`images/dog2.jpg`);
+  // dog3 = loadImage(`images/dog3.jpg`);
+  // dog4 = loadImage(`images/dog4.jpg`);
+}
 
 function setup() {
   createCanvas(600, 600);
-  background(200);
-  textSize(32)
-
-  text("click to randomize", 50, 50)
-
-  setTimeout(changeBackground, 1000);
+  background(0, 225, 255);
+  drawDogIntro();
+  textSize(32);
+  text("Name the Dogs!", width * .3, height * .18);
 
 }
 
@@ -31,25 +50,45 @@ function draw() {
 
 }
 
-function changeBackground() {
-  if (counter <= 4){
-    counter++;
-    console.log(counter)
-  background(random(255),random(255),random(255));
-  setTimeout(changeBackground, 1000);
-} else {
-  
+function drawDogIntro() {
+
+  image(introdogs, width * .1, height * .2);
+
 }
+
+
+function drawDog0() {
+  image(dog0, width * .1, height * .1)
+
+}
+
+function drawDog1() {
+  image(dog1, width * .1, height * .1)
+
+}
+
+function drawDog2() {
+  image(dog2, width * .13, height * .1)
+
+}
+
+function drawDog3() {
+  image(dog3, width * .1, height * .15)
+
+}
+
+function drawDog4() {
+  image(dog4, width * .09, height * .2)
+
 }
 
 function mousePressed() {
-  if (dogs[0]) {
-    background(random(200, 255));
-    randomIndex = int(random(dogs.length));
-    text(dogs[randomIndex].name, 50, 50);
-    dogs.splice(randomIndex, 1);
-  } else {
-    background(random(200, 255));
-    text("nothing left!", 50, 50);
+  if (dognames[0]) {
+    background(0, 225, 255);
+    randomIndex = int(random(dognames.length));
+    text(dognames[randomIndex].name, width * .43, height * .99);
+    randomImage = int(random(dogs.length));
+    image(dogs[randomImage], width * .1, height * .1);
+    dognames.splice(randomIndex, 0);
   }
 }
